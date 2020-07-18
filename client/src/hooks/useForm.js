79f9f926a8a46a79c1c0370e4ae4cbfd5f566346
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-07-17 20:45:42 -0500

    added the stateful logic to the useForm custom hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,26 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+// import { useLocalStorage } from "./useLocalStorage";
+
+export const useForm = (initialValue, callback) => {
+  const [values, setValues] = useState("form-data", initialValue);
+
+  const handleChanges = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    callback();
+  };
+
+  const clearForm = (e) => {
+    e.preventDefault();
+    setValues(initialValue);
+  };
+
+  return [values, handleChanges, handleSubmit, clearForm];
+};
